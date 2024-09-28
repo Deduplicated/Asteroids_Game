f6602e71f9e2d07c86fb6f332ec27aca965f313d
Robert Mullins <robert.mullins@klaviyo.com>
2024-09-28 23:11:14 +0100

    Added clock and also set the framerate to 60fps

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from constants import *
 import pygame
+from constants import *
+
+clock = pygame.time.Clock()
+dt = 0
 
 def main():
     pygame.init()
@@ -7,13 +10,13 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    i = 1
-    while i > 0:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
